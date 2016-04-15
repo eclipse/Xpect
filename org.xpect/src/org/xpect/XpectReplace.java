340da5a070826980a220d7be9b39ac95a744f645
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-15 14:10:37 +0200

    Added comments for Xpect Setup Annotations based on description in Wiki

--- org.xpect/src/org/xpect/XpectReplace.java
+++ org.xpect/src/org/xpect/XpectReplace.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * To replace any contribution (even the ones shipped with Xpect), you can annotate your own contribution with @XpectReplace(FooBar). FooBar being the contribution you want to
+ * replace. A contribution that has been replaced is not used any further by Xpect.
+ */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
