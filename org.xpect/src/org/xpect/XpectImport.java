0e1880c4920953feaa1b442487fce461efe4c350
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-15 14:10:37 +0200

    Added comments for Xpect Setup Annotations based on description in Wiki

--- org.xpect/src/org/xpect/XpectImport.java
+++ org.xpect/src/org/xpect/XpectImport.java
@@ -6,6 +6,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.xpect.runner.Xpect;
+import org.xpect.setup.XpectGuiceModule;
+import org.xpect.setup.XpectSetupComponent;
+import org.xpect.setup.XpectSetupFactory;
+import org.xpect.setup.XpectSetupRoot;
+
+/**
+ * This Annotation is used on a class with @Xpect methods to define what contributions should be present in the Xpect Test Setup. Possible contributions are classes annotated with
+ * these Xpect roles:
+ * <ul>
+ * <li>@{@link XpectSetupFactory}: Provides objects that can be injected into @Xpect methods and other contributions.</li>
+ * <li>@{@link XpectSetupComponent}: The class is available in the XPECT_SETUP section, if it is a parameter of some @XpectSetupRoot-annotated class' add(foo) method. These
+ * classes will also be proposed by content assist.</li>
+ * <li>@{@link XpectSetupRoot}: The types of the add(foo)-methods of this class are the types allowed as root-components in the XPECT_SETUP section.</li>
+ * <li>@{@link XpectGuiceModule} The annotated class is a google Guice module. The module will be mixed into the current language’s injector.</li>
+ * </ul>
+ * The @XpectImport annotation is valid on
+ * <ul>
+ * <li>all Xpect Java test classes</li>
+ * <li>on Java classes that are used as parameter types of @Xpect test methods</li>
+ * <li>on Java annotations that are used on parameter types on @Xpect test methods</li>
+ * <li>on Java classes that are contributions</li>
+ * <li>on super classes of any of the classes mentioned above.</li>
+ * </ul>
+ */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
--- org.xpect/src/org/xpect/state/Creates.java
+++ org.xpect/src/org/xpect/state/Creates.java
@@ -4,6 +4,12 @@ import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import org.xpect.setup.XpectSetupFactory;
+
+/**
+ * Used to mark factory methods in a {@link XpectSetupFactory}. Instances returned from these factory methods can be injected in Xpect test methods and setup contributions. An
+ * Annotation can be given to identify exactly one creation method/instance during injection. Otherwise, the matching is done by type.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Creates {
 	Class<? extends Annotation> value() default Default.class;
