c97428f69a18752f81cbeb69a22ce1712e87a7c2
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-15 15:03:07 +0200

    Added comments for Xpect Setup Annotations based on description in Wiki and own experience

--- org.xpect/src/org/xpect/setup/XpectSetupFactory.java
+++ org.xpect/src/org/xpect/setup/XpectSetupFactory.java
@@ -7,7 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.xpect.XpectContributionRole;
+import org.xpect.XpectImport;
 
+/**
+ * This annotation is used to mark a class as a XpectSetupFactory. The class must be imported in a test via @{@link XpectImport}.<br>
+ * All methods annotated with @Creates contribute objects to the Xpect Setup and can be injected into @Xpect methods and other setup contributions. The class is instantiated and
+ * managed by the StateContainer.
+ */
 @Inherited
 @XpectContributionRole
 @Retention(RetentionPolicy.RUNTIME)
--- org.xpect/src/org/xpect/setup/XpectSetupRoot.java
+++ org.xpect/src/org/xpect/setup/XpectSetupRoot.java
@@ -8,6 +8,12 @@ import java.lang.annotation.Target;
 
 import org.xpect.XpectContributionRole;
 
+/**
+ * Marks a class to be a XpectSetupRoot contribution to the Xpect Test Setup.<br>
+ * The types of the add(foo)-methods of this class are the types allowed as root-components in the XPECT_SETUP section. They should be @{@link XpectSetupComponent}s. The
+ * initialization will read the components from the XPECT_SETUP section and call the add(...) methods accordingly. Furthermore classes annotated with @XpectSetupRoot can be invoked
+ * with ISetupInitializer.initialize(T).
+ */
 @Inherited
 @XpectContributionRole
 @Retention(RetentionPolicy.RUNTIME)
