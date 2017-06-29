60a2bcf22133a90d4830e152f45b34fc67f428f6
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2017-06-29 20:39:39 +0200

    Configure Xtext language to be usable for testing
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@typefox.io>

--- org.xpect.xtext.lib.tests/src/org/xpect/xtext/lib/tests/util/grammaranalyzer/GrammarAnalyzerTest.java
+++ org.xpect.xtext.lib.tests/src/org/xpect/xtext/lib/tests/util/grammaranalyzer/GrammarAnalyzerTest.java
@@ -5,20 +5,21 @@ import java.util.List;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.Grammar;
 import org.junit.runner.RunWith;
+import org.xpect.XpectImport;
 import org.xpect.expectation.ILinesExpectation;
 import org.xpect.expectation.LinesExpectation;
 import org.xpect.runner.Xpect;
 import org.xpect.runner.XpectRunner;
-import org.xpect.setup.XpectSetup;
 import org.xpect.xtext.lib.setup.ThisModel;
 import org.xpect.xtext.lib.setup.XtextStandaloneSetup;
+import org.xpect.xtext.lib.setup.XtextWorkspaceSetup;
 import org.xpect.xtext.lib.util.GrammarAnalyzer;
 import org.xpect.xtext.lib.util.GrammarAnalyzer.CommentRule;
 
 import com.google.common.collect.Lists;
 
 @RunWith(XpectRunner.class)
-@XpectSetup(XtextStandaloneSetup.class)
+@XpectImport({ XtextStandaloneSetup.class, XtextWorkspaceSetup.class })
 public class GrammarAnalyzerTest {
 	@Xpect
 	public void comments(@LinesExpectation ILinesExpectation expectation, @ThisModel EObject grammar) {
