4bc9fb5e8260b1fd6baa38aa27d35e718d3f32a3
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2017-06-30 19:44:23 +0200

    Include information about test environment in test title
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@typefox.io>

--- org.xpect/src/org/xpect/runner/DescriptionFactory.java
+++ org.xpect/src/org/xpect/runner/DescriptionFactory.java
@@ -1,13 +1,16 @@
 package org.xpect.runner;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.util.XtextVersion;
 import org.junit.runner.Description;
 import org.xpect.XpectFile;
 import org.xpect.XpectInvocation;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 public class DescriptionFactory {
 
@@ -17,17 +20,17 @@ public class DescriptionFactory {
 		return result;
 	}
 
+	public static Description createFileDescription(String pathInProject, String fileName) {
+		String name = fileName + ": " + pathInProject;
+		Description result = Description.createSuiteDescription(name);
+		return result;
+	}
+
 	public static Description createFileDescription(XpectFile file) {
 		URI uri = file.eResource().getURI();
 		Preconditions.checkArgument(uri.isPlatform());
 		String pathInProject = Joiner.on('/').join(uri.segmentsList().subList(2, uri.segmentCount() - 1));
 		return createFileDescription(pathInProject, uri.lastSegment());
-	}
-
-	public static Description createFileDescription(String pathInProject, String fileName) {
-		String name = fileName + ": " + pathInProject;
-		Description result = Description.createSuiteDescription(name);
-		return result;
 	}
 
 	public static Description createFileDescriptionForError(Class<?> clazz, IXpectURIProvider uriProvider, URI uri) {
@@ -39,9 +42,9 @@ public class DescriptionFactory {
 
 	public static Description createTestDescription(Class<?> javaClass, IXpectURIProvider uriProvider, XpectInvocation invocation) {
 		URI uri = uriProvider.deresolveToProject(EcoreUtil.getURI(invocation));
-		TestTitleRegion titleRegion = invocation.getRelatedRegion(TestTitleRegion.class);
-		if (titleRegion != null && titleRegion.getTitle() != null)
-			return Description.createTestDescription(javaClass, uri.toString() + ": " + titleRegion.getTitle());
+		String title = getTitle(invocation);
+		if (!Strings.isNullOrEmpty(title))
+			return Description.createTestDescription(javaClass, uri.toString() + ": " + title);
 		else
 			return Description.createTestDescription(javaClass, uri.toString());
 	}
@@ -51,10 +54,31 @@ public class DescriptionFactory {
 		Preconditions.checkArgument(uri.isPlatform());
 		String className = invocation.getFile().getJavaModel().getTestOrSuite().getJvmClass().getQualifiedName();
 		String text = Joiner.on('/').join(uri.segmentsList().subList(2, uri.segmentCount())) + "#" + uri.fragment();
-		TestTitleRegion titleRegion = invocation.getRelatedRegion(TestTitleRegion.class);
-		if (titleRegion != null && titleRegion.getTitle() != null)
-			return Description.createTestDescription(className, text + ": " + titleRegion.getTitle());
+		String title = getTitle(invocation);
+		if (!Strings.isNullOrEmpty(title))
+			return Description.createTestDescription(className, text + ": " + title);
 		else
 			return Description.createTestDescription(className, text);
+	}
+
+	public static String getTitle(XpectInvocation inv) {
+		String postfix = System.getProperty("xpectTestTitlePostfix");
+		String env = System.getProperty("xpectTestTitleShowEnvironment");
+		TestTitleRegion titleRegion = inv.getRelatedRegion(TestTitleRegion.class);
+		StringBuilder result = new StringBuilder();
+		if (titleRegion != null) {
+			String title = titleRegion.getTitle();
+			if (!Strings.isNullOrEmpty(title)) {
+				result.append(title);
+			}
+		}
+		if (!Strings.isNullOrEmpty(postfix)) {
+			result.append(postfix);
+		}
+		if (!Strings.isNullOrEmpty(env)) {
+			result.append(EcorePlugin.IS_ECLIPSE_RUNNING ? ".plugin" : ".standalone");
+			result.append(".xtext_" + XtextVersion.getCurrent().getVersion());
+		}
+		return result.toString();
 	}
 }
