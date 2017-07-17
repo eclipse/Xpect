41252e4b65f8444c4b88e11d982f7bd6767b86d7
mmews <marcus.mews@numberfour.eu>
2017-07-17 11:58:23 +0200

    added copyright header everywhere

--- org.xpect/src/org/xpect/text/IRegion.java
+++ org.xpect/src/org/xpect/text/IRegion.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 TypeFox GmbH and itemis AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Moritz Eysholdt - Initial contribution and API
+ *******************************************************************************/
+
 package org.xpect.text;
 
 /**
